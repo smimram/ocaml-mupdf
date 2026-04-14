0390602b1d78d66d449f5c93b915ba1a36da8f06
Samuel Mimram <smimram@gmail.com>
2026-04-14 12:59:37 +0200

    Test.

--- test/test.ml
+++ test/test.ml
@@ -3,4 +3,6 @@ open Mupdf.MuPDF
 let () =
   Document.register_handlers ();
   let doc = Document.open_document "test.pdf" in
-  Printf.printf "Pages: %d\n%!" @@ Document.count_pages doc
+  Printf.printf "Pages: %d\n%!" @@ Document.count_pages doc;
+  let _page = Document.load_page doc 0 in
+  ()
